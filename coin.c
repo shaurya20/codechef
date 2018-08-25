dccc401b5bc869a57a4fdd661f0c4a15c3c43cab
shaurya20 <jain19shaurya@gmail.com>
2018-08-26 03:31:39 +0530

    Add files via upload

---
+++ coin.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int large(int arr[],int m,int n){
+int max;
+max=arr[0];
+for(i=0;i<n;i++){
+if(arr[i]>max)
+max=arr[i];
+
+}
+return max;
+}
+int main(){
+int arr[100],int k,l,will;
+scanf("%d",&T);
+scanf("%d",&k);
+for(i=0;i<k;i++){
+scanf("%d",&a[i]);
+}
+will=large;
+for(i=0;i<n;i++){
+temp=arr[i+1]-arr[i];
+if(temp<min){
+min=temp;
+printf("%d",&min);
+return 0;
+}
+}
+
+}
